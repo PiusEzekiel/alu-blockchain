b07c645ed34d292a81338ca50cf16f9aafb6defb
piusezekiel <e.pius1@alustudent.com>
2025-01-16 09:40:33 +0200

    fix: cast buffer to char pointer in blockchain_create function

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -42,7 +42,7 @@ blockchain_t *blockchain_create(void)
     genesis_block->info.nonce = 0;
     memset(genesis_block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
 
-    strncpy(genesis_block->data.buffer, "Holberton School", 16);
+    strncpy((char *)genesis_block->data.buffer, "Holberton School", 16);
     genesis_block->data.len = 16;
 
     memcpy(genesis_block->hash, "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03", SHA256_DIGEST_LENGTH);
