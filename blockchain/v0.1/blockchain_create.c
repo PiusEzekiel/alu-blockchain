1b7002609404328c1ed1de461ed61bd052f16918
piusezekiel <e.pius1@alustudent.com>
2025-01-16 10:00:46 +0200

    fix: improve error handling in blockchain_create function

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -12,19 +12,21 @@ blockchain_t *blockchain_create(void)
 {
     blockchain_t *blockchain;
     block_t *genesis_block;
-
+    
     blockchain = malloc(sizeof(blockchain_t));
     if (!blockchain)
         return (NULL);
 
     blockchain->chain = llist_create(MT_SUPPORT_FALSE);
-    if (!blockchain->chain) {
+    if (!blockchain->chain)
+    {
         free(blockchain);
         return (NULL);
     }
 
     genesis_block = calloc(1, sizeof(block_t));
-    if (!genesis_block) {
+    if (!genesis_block)
+    {
         llist_destroy(blockchain->chain, 1, NULL);
         free(blockchain);
         return (NULL);
@@ -43,9 +45,8 @@ blockchain_t *blockchain_create(void)
            "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c"
            "\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03",
            SHA256_DIGEST_LENGTH);
-
-    /* Add the genesis block to the blockchain */
-    if (llist_add_node(blockchain->chain, genesis_block, ADD_NODE_REAR) !=
+           
+        if (llist_add_node(blockchain->chain, genesis_block, ADD_NODE_REAR) !=
         0) {
         free(genesis_block);
         llist_destroy(blockchain->chain, 1, NULL);
